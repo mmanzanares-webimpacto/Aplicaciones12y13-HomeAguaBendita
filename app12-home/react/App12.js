f9c5717617c5fd8a86a186186f737ca417dc7fc9
mmanzanareswebimpacto <50134749+mmanzanareswebimpacto@users.noreply.github.com>
2019-05-22 17:16:22 +0200

    Add files via upload

--- app12-home/react/App12.js
+++ app12-home/react/App12.js
@@ -5,7 +5,8 @@ import classnames from 'classnames';
 
 export default class App15 extends Component {
     static defaultProps = {
-        images: [],
+        blockimages1: [],
+        blockimages2: [],
         texto: "texto"
     }
 
@@ -24,9 +25,38 @@ export default class App15 extends Component {
             title: 'Info Products Home',
             type: 'object',
             properties: {
-                images: {
+                blockimages1: {
                     minItems: 1,
-                    title: 'Items',
+                    title: 'Bloque imagenes 1',
+                    type: 'array',
+                    items: {
+                        title: 'Item',
+                        type: 'object',
+                        properties: {
+                            image: {
+                                default: '',
+                                title: 'Imagen',
+                                type: 'string',
+                                widget: {
+                                    'ui:widget': 'image-uploader',
+                                },
+                            },
+                            texto: {
+                                title: 'Texto de la imagen',
+                                type: 'string',
+                                default: 'Texto de la imagen'
+                            },
+                            link: {
+                                title: 'Link',
+                                type: 'string',
+                                default: 'Link del producto'
+                            }
+                        },
+                    },
+                },
+                blockimages2: {
+                    minItems: 1,
+                    title: 'Bloque imagenes 2',
                     type: 'array',
                     items: {
                         title: 'Item',
@@ -59,39 +89,35 @@ export default class App15 extends Component {
 
     render() {
         const {
-            images
+            blockimages1,
+            blockimages2
         } = this.props
-
+        
         return (
-            <Grid>
+            <Grid className={(styles.abs, "mt9")}>
                 <Row>
-                    <Col sm={6} className={classnames(styles.col)}>
-                        <img src={images[0].image}></img>
-                        <span className={classnames(styles.texto)}>{images[0].texto}</span>
-                    </Col>
                     <Col sm={6}>
-                        <Row>
-                            <Col sm={6} className={classnames(styles.col)}>
-                                <img src={images[1].image}></img>
-                                <span className={classnames(styles.texto1)}>{images[1].texto}</span>
-                            </Col>
-                            <Col sm={6} className={classnames(styles.col)}>
-                                <img src={images[2].image}></img>
-                                <span className={classnames(styles.texto1)}>{images[2].texto}</span>
-                            </Col>
-                            <Col sm={6} className={classnames(styles.col)}>
-                                <img src={images[3].image}></img>
-                                <span className={classnames(styles.texto1)}>{images[3].texto}</span>
-                            </Col>
-                            <Col sm={6} className={classnames(styles.col)}>
-                                <img src={images[4].image}></img>
-                                <span className={classnames(styles.texto1)}>{images[4].texto}</span>
-                            </Col>
+                        <Row className={styles.centrar}>
+                            {blockimages1.map((d, i) =>
+                                <Col sm={i==(blockimages1.length-1) ? (blockimages1.length%2==0 ? 6 : 12) : 6} className={classnames(styles.col)}>
+                                    <img className={i==(blockimages1.length-1) ? (blockimages1.length%2!=0 ? styles.cortar : "") : ""} src={d.image}></img>
+                                    <span className={classnames(styles.texto)}>{d.texto}</span>
+                                </Col>
+                            )} 
                         </Row>
                     </Col>
-                </Row>
+                    <Col sm={6}>
+                        <Row className={styles.centrar}>
+                            {blockimages2.map((d, i) => 
+                                <Col sm={i==(blockimages2.length-1) ? (blockimages2.length%2==0 ? 6 : 12) : 6}   className={classnames(styles.col)}>
+                                    <img src={d.image}></img>
+                                    <span className={classnames(styles.texto1)}>{d.texto}</span>
+                                </Col>
+                            )}
+                        </Row>
+                    </Col>
+                </Row>  
             </Grid>
         );
-        //{classnames(styles.col)}
     }
 }
